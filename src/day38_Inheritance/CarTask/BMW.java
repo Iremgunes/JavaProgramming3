5711eb1a22bb1884790ee8a1a3765608381fcb2c
DELL-ırem <aviremdanacioglu@gmail.com>
2022-04-11 12:41:06 +0300

    april eleven commit 3

--- src/day38_Inheritance/CarTask/BMW.java
+++ src/day38_Inheritance/CarTask/BMW.java
@@ -2,17 +2,18 @@ package day38_Inheritance.CarTask;
 
 public class BMW extends Car {
 
-    public BMW(String brand, String model, int year, double price, String color, int miles) {
+    public BMW(String model, int year, double price, String color, int miles) {
         super("BMW", model, year, price, color, miles);
     }
 
     public void breaksDown(){
-
+        System.out.println(brand + " " + model +  " breaks down evry 100 miles");
     }
 
     public void racing(){
-
+        System.out.println(brand + " " + model + " is racing");
     }
+
 
 
 
--- src/day38_Inheritance/CarTask/Tesla.java
+++ src/day38_Inheritance/CarTask/Tesla.java
@@ -2,7 +2,14 @@ package day38_Inheritance.CarTask;
 
 public class Tesla extends Car {
 
-    public Tesla(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
     }
+
+    public void autoPilot(){
+        System.out.println(brand + " " + model + " in autopilot mode");
+    }
+
+
+
 }
