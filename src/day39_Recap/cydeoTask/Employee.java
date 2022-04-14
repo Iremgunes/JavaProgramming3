86e119ac2ab97ea55b70a3214e23bc81bd4fdfe3
DELL-ırem <aviremdanacioglu@gmail.com>
2022-04-14 12:20:48 +0300

    april fourteen commit 2

---
+++ src/day39_Recap/cydeoTask/Employee.java
@@ -0,0 +1,13 @@
+package day39_Recap.cydeoTask;
+
+public class Employee extends Person{
+
+
+
+    public Employee(String name, int age, char gender) {
+        super(name, age, gender);
+    }
+
+
+
+}
