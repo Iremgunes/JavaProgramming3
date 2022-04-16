e7b7ebc92e526f88f309a6f678beab246d58f452
DELL-ırem <aviremdanacioglu@gmail.com>
2022-04-16 13:11:18 +0300

    april sixteen commit 2

---
+++ src/day40_FinalKeyword/FinalKeyword.java
@@ -0,0 +1,11 @@
+package day40_FinalKeyword;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
--- src/utilities/AccessModifiers_Test4.java
+++ src/utilities/AccessModifiers_Test4.java
@@ -1,4 +1,15 @@
 package utilities;
 
-public class AccessModifiers_Test4 {
+import day40_FinalKeyword.ProtectedAccesModifiers;
+
+public class AccessModifiers_Test4 extends ProtectedAccesModifiers {
+
+    public static void main(String[] args) {
+
+        System.out.println(AccessModifiers_Test4.name2);
+     //   System.out.println(AccessModifiers_Test4.name1);
+
+
+    }
+
 }
