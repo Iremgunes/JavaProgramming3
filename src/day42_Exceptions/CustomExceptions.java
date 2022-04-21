70ec45d0645de32f6ef4c38113e2920d0e8792c1
DELL-ırem <aviremdanacioglu@gmail.com>
2022-04-21 13:54:54 +0300

    april twenty one commit 5

---
+++ src/day42_Exceptions/CustomExceptions.java
@@ -0,0 +1,34 @@
+package day42_Exceptions;
+
+import com.sun.tools.javac.Main;
+
+class FadyException extends RuntimeException{// UNCHECKED EXCEPTION
+
+    public FadyException(){
+        super("time for a short break");
+    }
+
+    public FadyException(String message) {
+        super(message);
+       // System.out.println("it is time for break");
+    }
+}
+
+class NoBreakException extends Exception{//CHECKED EXCEPTİON
+
+}
+
+public class CustomExceptions {
+
+    public static void main(String[] args) {
+
+      //  throw new FadyException();
+
+        try {
+            throw new NoBreakException();
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
