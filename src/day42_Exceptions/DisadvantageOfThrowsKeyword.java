43cbd8772ab456a84950a3ca7c78593b8b06a3e5
DELL-ırem <aviremdanacioglu@gmail.com>
2022-04-21 12:45:54 +0300

    april twenty one commit 1

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword.java
@@ -1,10 +1,23 @@
 package day42_Exceptions;
 
 public class DisadvantageOfThrowsKeyword {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("hello");
 
+        sleep(2.5);
 
+        System.out.println("hello world");
 
+        System.out.println("---------------------");
 
+        System.out.println("hello");
+
+        MorningWorkOut.sleep(2.5);
+
+        System.out.println("cydeo");
+    }
+
+    public static void sleep(double seconds) throws InterruptedException {
+        Thread.sleep((long) (seconds*1000));
     }
 }
