17710bdf16d1aded1d5ea68ba17d3a2530d5bdaf
Iremgunes <aviremdanacioglu@gmail.com>
2022-04-29 15:30:52 +0300

    april twenty nine commit 3

--- src/day43_Abstraction/EmployeeTask/Employee.java
+++ src/day43_Abstraction/EmployeeTask/Employee.java
@@ -2,17 +2,50 @@ package day43_Abstraction.EmployeeTask;
 
 public abstract class Employee extends Person {
 
-    private String name;
-    private int age;
-    private  char gender;
     private final int ID;
     private String jobTitle;
     private double salary;
 
 
+    public Employee(String name, int age, char gender, int ID, String jobTitle, double salary) {
+        super(name, age, gender);
+        this.ID = ID;
+        setJobTitle(jobTitle);
+        setSalary(salary);
+    }
 
+    public int getID() {
+        return ID;
+    }
 
+    public String getJobTitle() {
+        return jobTitle;
+    }
 
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 
+    public double getSalary() {
+        return salary;
+    }
 
+    public void setSalary(double salary) {
+
+        this.salary = salary;
+    }
+
+    public abstract void work();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name=" + getName() +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", ID=" + ID +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
